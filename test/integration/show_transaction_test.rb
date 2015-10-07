dbe4c9654167ae413619e613c774284df8016b61
Kirill Sukhorukov <kirill.suhorukov@gmail.com>
2015-10-08 00:09:46 +0300

    handling unexisting transactions gracefully

--- test/integration/show_transaction_test.rb
+++ test/integration/show_transaction_test.rb
@@ -9,4 +9,13 @@ class ShowTransactionTest < ActionDispatch::IntegrationTest
     assert_equal result,  { "amount" => 6000.0, "type" => "cars"}
   end
 
+  test 'handling unexisting transactions gracefully' do 
+    get '/transactionservice/transaction/12345'
+    assert_equal 200, response.status
+    assert_equal Mime::JSON, response.content_type
+
+    result = JSON.parse(response.body)
+    assert_equal result,  { "Error" =>  "There is no such transaction"}
+  end
+
 end
